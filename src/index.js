d4f175202b9f80af5433b9c01e3567c75a8e77a2
여율 <dbfm2222@gmail.com>
2021-02-01 23:18:58 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+class ObjectUtilities {
+  /* Your magic here */
+  static mergeObjects = (objA, objB) => ({ ...objA, ...objB });
+  static removePassword = (user) => delete user.password;
+  static freezeObj = (cleanUser) => {
+    Object.freeze(cleanUser);
+  };
+  static getOnlyValues = (frozenUser) => ();
+  static getOnlyProperties = (frozenUser) => {};
+}
+
+const objA = {
+  name: "Nicolas",
+  favFood: "Kimchi"
+};
+
+const objB = {
+  password: "12345"
+};
+
+const user = ObjectUtilities.mergeObjects(objA, objB);
+console.log(user);
+
+const cleanUser = ObjectUtilities.removePassword(user);
+console.log(cleanUser);
+
+const frozenUser = ObjectUtilities.freezeObj(cleanUser);
+
+const onlyValues = ObjectUtilities.getOnlyValues(frozenUser);
+console.log(onlyValues);
+
+/*const onlyProperties = ObjectUtilities.getOnlyProperties(frozenUser);
+console.log(onlyProperties);*/
+
+// frozenUser.name = "Hello!"; // This should show an error
